8f29a03ba2377e99f8b7e1a058788a7dff89895c
shasky2014 <249398363@qq.com>
2018-11-22 14:23:16 +0800

    写了几个小demo

--- operate_mysql/select_query.py
+++ operate_mysql/select_query.py
@@ -25,13 +25,11 @@ print("-----------------------------")
 cnx = mysql.connector.connect(host='localhost',
                               user='root',
                               passwd='1',
-                              database='order_settle'
+                              database='test'
                               )
 cursor = cnx.cursor()
-query =("SELECT DATE_FORMAT( order_time, '%Y-%m' ) as aa , count( 1 ) as cc "
-        "FROM excel_order_detail "
-        "GROUP BY DATE_FORMAT( order_time, '%Y-%m' ); "
-        )
+
+query =("select * from `dual` where id ='22222'")
 cursor.execute(query)
 for (aa,cc ) in cursor:
     print(aa,cc)
